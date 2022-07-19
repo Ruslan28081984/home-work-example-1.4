a824cd5693cf8c296d1554f99e774250ed3935ff
user1 <89201828020@mail.ru>
2022-07-19 15:57:38 +0300

    Сделал 3 задачи.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //examples1();
+        examples2();
+    }
+    private static void examples1(){
+        int i = 0;
+        while (i < 10){
+            i ++;
+            System.out.print( i + " " );
+        }
+            System.out.println();
+            for (i = 10; i > 0; i --) {
+                System.out.print(i + " " );
+            }
+    }
+    private static void examples2(){
+        int friday = 1;
+        for (int i = 1; i < 31; i = i + 7) {
+            System.out.println(" Сегодня пятница, " + i + " число. Необходимо подготоить отчёт");
+        }
+}
+    private static void examples3(){
+
     }
 }
